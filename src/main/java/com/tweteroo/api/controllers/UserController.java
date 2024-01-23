f7fbc3832a0b127d6e84de7adf01e09dbda78cd6
422UR4H <tiagosindra@id.uff.br>
2024-01-23 12:04:04 -0300

    :sparkles: Feat: delete user

--- src/main/java/com/tweteroo/api/controllers/UserController.java
+++ src/main/java/com/tweteroo/api/controllers/UserController.java
@@ -12,11 +12,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("users")
@@ -40,11 +43,16 @@ public class UserController {
   @GetMapping("/{id}")
   public Optional<UserModel> getUserById(@PathVariable("id") @NonNull Long id) {
     Optional<UserModel> user = userRepository.findById(id);
-    
+
     if (!user.isPresent()) {
       return Optional.empty();
     }
     return Optional.of(user.get());
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteUser(@PathVariable("id") @NonNull Long id) {
+    userRepository.deleteById(id);
   }
 
 }
