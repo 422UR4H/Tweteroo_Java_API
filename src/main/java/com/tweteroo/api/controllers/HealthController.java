ae750ccdf38a8104ed5d0969735ae40bfcbaede8
422UR4H <tiagosindra@id.uff.br>
2024-01-30 18:43:25 -0300

    :sparkles: Fix: add cors

--- src/main/java/com/tweteroo/api/controllers/HealthController.java
+++ src/main/java/com/tweteroo/api/controllers/HealthController.java
@@ -1,8 +1,10 @@
 package com.tweteroo.api.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HealthController {
   @GetMapping("health")
